285f28223645a107967f8c6f01e108e537ad6944
devsanthoshmk <connectwithsanthoshmk@gmail.com>
2025-09-05 15:45:05 +0530

    homepage fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -67,8 +67,8 @@
         </div>
         
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <ProductCard v-for="(product, id) in paginatedProducts" :key="product.id" :product="{ ...product }" data-aos="fade-up"
-          :data-aos-delay="100 * (Object.keys(paginatedProducts).indexOf(id) % 8)" />
+          <ProductCard v-for="(product, id) in featuredProducts" :key="product.id" :product="{ ...product }" data-aos="fade-up"
+          :data-aos-delay="100 * (Object.keys(featuredProducts).indexOf(id) % 8)" />
         </div>
         
         <div class="text-center mt-12" data-aos="fade-up" data-aos-delay="500">
